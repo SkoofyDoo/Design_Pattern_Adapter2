703a6e5babfb70a443aea7c843023b7177f835dc
EVGENY KVEST <evgenykvest@gmail.com>
2025-01-09 08:48:10 +0100

    Optimierung der Aufgabe

--- src/art/ModernPrinterImpl.java
+++ src/art/ModernPrinterImpl.java
@@ -5,7 +5,7 @@ public class ModernPrinterImpl implements ModernPrinter{
 
     @Override
     public void  printFormatted(String message, String font, String color) {
-        System.out.println("New Connected: " + message + "Font: " + font + "Color: " + color);
+
     }
 }
 
